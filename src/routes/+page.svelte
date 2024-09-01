dfe25d36b49ec8c4ac0b0220eda3f2a1bba265c1
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-01 18:05:14 +0530

    feat: using WASM Node.js module adapter-static used

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { browser } from "$app/environment";
+    import init, { greet } from 'wasm-js-rust';
+	import { onMount } from "svelte";
+
+    $: greetings = ""
+    onMount(async () => {
+        await init(); // init initializes memory addresses needed by WASM and that will be used by JS/TS
+        greetings = greet("User");
+    })
+</script>
+
+<h1>State</h1>
+<p>Greetings: {greetings}</p>
+<p>Browser rendered: {browser}</p>
---
+++ src/routes/+page.ts
@@ -0,0 +1,3 @@
+export const prerender = true;
+// export const ssr = false; // No SSR possible in adapter-static
+// export const csr = false;
