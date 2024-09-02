0e167dcb586d0d05eed7f6e561a3b3f90a5a8b1c
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-02 22:55:19 +0530

    add: receive SSE test events sse.dev/test as external source

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,27 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang=ts>
+    import { writable } from "svelte/store";
+
+    type SSE_Event = {
+        msg: string;
+        now: string;
+    };
+
+    const messages = writable<SSE_Event[]>([]);
+
+    const sse_url = "https://sse.dev/test";
+    const evtSource = new EventSource(sse_url);
+
+    evtSource.onmessage = (event) => {
+        // event.data is a string
+        //data: {"testing":true,"sse_dev":"is great","msg":"It works!","now":1725297712676}\n\n
+        const event_data: SSE_Event = JSON.parse(event.data);
+        messages.update(prev => prev.concat(event_data));
+    };
+</script>
+
+<h1>Server Side Events</h1>
+<p>Source {sse_url}</p>
+
+{#each $messages as message}
+    <p>{message.msg} - {message.now}</p>    
+{/each}
