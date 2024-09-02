592fd2742fc83a70ca2cec388541e14452c7da77
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-03 00:12:57 +0530

    add: SSR enabled with some SSR cached events.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,28 @@
 <script lang=ts>
     import { writable } from "svelte/store";
+    import { browser } from "$app/environment";
 
     type SSE_Event = {
         msg: string;
-        now: string;
+        now: number;
     };
 
-    const messages = writable<SSE_Event[]>([]);
+    const messages = writable<SSE_Event[]>([
+        {"msg":"SSR Cache 1","now":1725299770406},
+        {"msg":"SSR Cache 2","now":1725299770507}
+    ]);
 
-    const sse_url = `http://${window.location.host}/sse-source`;
-    const evtSource = new EventSource(sse_url);
+    const sse_url = `/sse-source`;
 
-    evtSource.onmessage = (event) => {
-        //data: {"msg":"Message from instance: 1","now":1725299770405}\n\n
-        const event_data: SSE_Event = JSON.parse(event.data);
+    if(browser) {
+        messages.set([]);
+        const evtSource = new EventSource(sse_url);
+        evtSource.onmessage = (event) => {
+            //data: {"msg":"Message from instance: 1","now":1725299770405}\n\n
+            const event_data: SSE_Event = JSON.parse(event.data);
         messages.update(prev => prev.concat(event_data));
-    };
+    }
+};
 </script>
 
 <h1>Server Side Events</h1>
