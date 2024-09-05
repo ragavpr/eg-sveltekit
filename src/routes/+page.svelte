b7c03e8173dccfebfbb4965997b9d57503dd958c
Ragavendaran Puliyadi <ragavendaran.puli@gmail.com>
2024-09-05 12:56:49 +0530

    impl: sveltekit-sse example

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	// src/routes/+page.svelte
+	import { source } from 'sveltekit-sse';
+	const value = source('/sse-source').select('message');
+</script>
+
+{$value}
